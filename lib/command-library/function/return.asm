9e5fd13990cb6b5f38b8355c1c8adb30160725c7
ijrex <iain.rex@googlemail.com>
2020-08-22 14:25:38 +0100

    feat(functions): Clean-up return command

--- lib/command-library/function/return.asm
+++ lib/command-library/function/return.asm
@@ -1,12 +1,6 @@
 // FRAME = LCL
 @LCL
 D=M
-@R15
-M=D
-
-// RET = *(FRAME - 5)
-@5
-D=D-A
 @R14
 M=D
 
@@ -25,37 +19,39 @@ D=M
 M=D+1
 
 // THAT = *(FRAME-1)
-@R15
-MD=M-1
+@R14
+M=M-1
 A=M
 D=M
 @THAT
 M=D
 
 // THIS = *(FRAME-2)
-@R15
-MD=M-1
+@R14
+M=M-1
 A=M
 D=M
 @THIS
 M=D
 
 // ARG = *(FRAME-3)
-@R15
-MD=M-1
+@R14
+M=M-1
 A=M
 D=M
 @ARG
 M=D
 
 // LCL = *(FRAME-4)
-@R15
-MD=M-1
+@R14
+M=M-1
 A=M
 D=M
 @LCL
 M=D
 
-// GOTO RET
+// RETURN ADDRESS = *(FRAME-5)
 @R14
+M=M-1
+A=M
 0;JMP
